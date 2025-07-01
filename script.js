08c3f9276a9b5c706bfffb498979a26fef398082
SRE-NADH <95708236+SRE-NADH@users.noreply.github.com>
2025-07-01 19:26:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+const input = document.getElementById("evaluatedText");
+const h3 = document.getElementById("letterCount");
+let count =0;
+
+input.addEventListener('input',(event)=>{
+	count = event.target.value.length;
+	h3.textContent = count;
+})
+
+
